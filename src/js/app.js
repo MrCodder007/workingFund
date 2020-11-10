67e98c17658bf26d0af7f2be5d440c7b44ae3921
MrCodder007 <akubovalatifa@gmail.com>
2020-11-11 01:50:26 +0200

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import multiItemSlider from './lib/multiItemSlider';
+import sayHello from './lib/sayHello';
+import scrolling from './lib/scrolling';
+
+multiItemSlider();
+sayHello();
+scrolling();
