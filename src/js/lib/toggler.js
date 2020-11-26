41e3bd014ccb584666f6730bc10594cf4c41ad91
MrCodder007 <akubovalatifa@gmail.com>
2020-11-26 14:21:39 +0200

    js

--- src/js/lib/toggler.js
+++ src/js/lib/toggler.js
@@ -17,21 +17,13 @@ menuLinks.forEach(
   }
 )
 
-// var navbar = document.querySelector(".nav-menu__contacts")
-// var ham = document.querySelector(".nav-menu__toggler")
+var list = document.querySelector(".plans-list__window")
+var listBottom = document.querySelector(".plans-list__window--hover")
 
-// // toggles hamburger menu in and out when clicking on the hamburger
-// function toggleHamburger(){
-//   navbar.classList.toggle("nav-menu__contacts--active")
-//   ham.classList.toggle("nav-menu__toggler--active")
-// }
-
-// ham.addEventListener("click", toggleHamburger)
-
-// // toggle when clicking on links
-// var menuLinks = document.querySelectorAll(".menuLink")
-// menuLinks.forEach( 
-//   function(menuLink) { 
-//     menuLink.addEventListener("click", toggleHamburger) 
-//   }
-// )
+$('.plans-list__window').click( function(){
+  if ( $(this).hasClass('plans-list__window--hover') ) {
+      $(this).removeClass('plans-list__window--hover');
+  } else {
+      $(this).addClass('plans-list__window--hover');    
+  }
+});
