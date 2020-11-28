fb9e1b875474cbf47982902c70528ab8179da7a2
MrCodder007 <akubovalatifa@gmail.com>
2020-11-28 17:04:56 +0200

    asdsad

---
+++ src/js/lib/contacts-toggler.js
@@ -0,0 +1,13 @@
+$('.nav-menu__toggler').click(function () {
+	$('.overlay').toggleClass('shown');
+	$('.nav-menu').toggleClass('nav-menu__toggler--active');
+});
+
+
+$('.overlay').click(function (evt) {
+	const $overlay = $(evt.currentTarget);
+	if ($overlay.hasClass('shown')) {
+		$overlay.removeClass('shown');
+		$('.nav-menu').removeClass('nav-menu--active');
+	}
+});
